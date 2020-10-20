308f3876995bfae590a6818480f639f95f6c7a86
vmarci80 <wmtech.vetromarton@gmail.com>
2020-10-20 12:21:38 +0000

    minedn jo

--- f0003a.py
+++ f0003a.py
@@ -1,2 +1,9 @@
 név = input('Hogy hívnak téged? ')
 print('Szervusz ', név, ', örülök, hogy találkoztunk.', sep='')
+szuletesi_ev = input('Mi a születési éved? ')
+csillagjegy = input('Mi a csillagjegyed? ')
+print('Adatok:')
+print('Neved: ', név ,)
+print('Születési időd: ',szuletesi_ev)
+print('Csillagjegyed: ', csillagjegy)
+
